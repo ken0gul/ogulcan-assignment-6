441040a7d6046193a8a7e8b0f39f282263a31287
TheJohnCodes <93669966+ken0gul@users.noreply.github.com>
2022-10-04 16:06:02 -0400

    Add missing year 2016 to each model

--- src/com/coderscampus/assignment6/AnalyzeData.java
+++ src/com/coderscampus/assignment6/AnalyzeData.java
@@ -20,18 +20,21 @@ public class AnalyzeData {
 		// Create 3 different List for 3 different year
 		List<Car> car = modelName.getSales();
 		
+		List<Car> sales16 = getYearOfTheSales(car, 2016);
 		List<Car> sales17 = getYearOfTheSales(car, 2017);
 		List<Car> sales18 = getYearOfTheSales(car, 2018);
 		List<Car> sales19 = getYearOfTheSales(car, 2019);
-		// Total sales of Tesla model3 in 2017
+
+		Integer totalSales16 = sales16.stream()
+									
+									  .collect(Collectors.summingInt(i -> i.getNumberOfSales()));
+		
 		Integer totalSales17 = sales17.stream()
 								 .collect(Collectors.summingInt(i -> i.getNumberOfSales()));
 		
-		// Total sales of Tesla model3 in 2018 
 		Integer totalSales18 = sales18.stream()
 				 .collect(Collectors.summingInt(i -> i.getNumberOfSales()));
 		
-		// Total sales of Tesla modelX in 2019
 		Integer totalSales19 = sales19.stream()
 				 .collect(Collectors.summingInt(i -> i.getNumberOfSales()));
 		
@@ -47,16 +50,16 @@ public class AnalyzeData {
 				   .get().format(DateTimeFormatter.ofPattern("MMMM-yyyy"));
 		// Print it out
 		
-		printHello(model, totalSales17, totalSales18, totalSales19, bestMonth, worstMonth);
+		printHello(model, totalSales16,totalSales17, totalSales18, totalSales19, bestMonth, worstMonth);
 		
 		
 		return car;
 	}
 	
 	
-	public static void printHello(String model, Integer totalSales17, Integer totalSales18, Integer totalSales19, String bestMonth, String worstMonth ) {
+	public static void printHello(String model, Integer totalSales16,Integer totalSales17, Integer totalSales18, Integer totalSales19, String bestMonth, String worstMonth ) {
 		System.out.println(model + " Yearly Sales Report");
-		System.out.println("2017 -> " + "$"+totalSales17 + "\n"
+		System.out.println("2016 -> $" + totalSales16 +"\n2017 -> " + "$"+totalSales17 + "\n"
 				+ "2018 -> " +"$"+ totalSales18 + "\n2019 -> " + "$"+totalSales19);
 		System.out.println("The best month for Model S was: " + bestMonth
 				+ "\nThe worst month for Model S was: " + worstMonth +"\n");
